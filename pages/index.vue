998eb04ff117370efb681c5bdff623dd78b2d486
Tobias Blum <tobias.blum@besonet.ch>
2023-04-29 22:04:10 +0200

    Add first naive ui element

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,29 @@
     <button @click="logout">
       logout
     </button>
+    <div>
+      <NDynamicTags v-model:value="tags" :max="3" />
+      {{ tags }}
+    </div>
+    <button @click="addTag">
+      add
+    </button>
+
+    <br>
+    <br>
+
     <pre>{{ auth }}</pre>
   </div>
 </template>
 
 <script setup lang="ts">
+
 const auth = useAuth()
 const { loginWithEmail, logout } = useAuthClient()
+
+const tags = ref(['teacher', 'programmer'])
+
+const addTag = () => {
+  tags.value.push('nope')
+}
 </script>
