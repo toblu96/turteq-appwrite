2bbf65b738b3f9ac08627bbb4e54e9a65e73d834
Tobias Blum <tobias.blum@besonet.ch>
2023-05-01 17:34:55 +0200

    Avatar group improvements

--- components/tt/avatar/group/ProjectMembers.vue
+++ components/tt/avatar/group/ProjectMembers.vue
@@ -9,7 +9,7 @@
       </NTooltip>
     </template>
     <template #rest="{ options: restOptions, rest }">
-      <NDropdown :options="createDropdownOptions(restOptions)" placement="top">
+      <NDropdown :options="createDropdownOptions(restOptions)" placement="top" :show-arrow="true">
         <NAvatar>+{{ rest }}</NAvatar>
       </NDropdown>
     </template>
@@ -20,24 +20,24 @@
 
 const options = ref([
   {
-    name: 'Leonardo DiCaprio',
-    src: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg'
+    name: 'Leslie Alexander',
+    src: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
   },
   {
-    name: 'Jennifer Lawrence',
-    src: 'https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg'
+    name: 'Michael Foster',
+    src: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
   },
   {
-    name: 'Audrey Hepburn',
-    src: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg'
+    name: 'Dries Vincent',
+    src: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
   },
   {
-    name: 'Anne Hathaway',
-    src: 'https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg'
+    name: 'Lindsay Walton',
+    src: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
   },
   {
-    name: 'Taylor Swift',
-    src: 'https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg'
+    name: 'Courtney Henry',
+    src: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
   }
 ])
 
