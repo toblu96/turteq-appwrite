15d56f2dbd60264e1f317d4684be1f137502decb
Tobias Blum <tobias.blum@besonet.ch>
2023-05-01 22:40:35 +0200

    Fix styling issue on user dropdown

--- components/tt/dropdown/UserProfile.vue
+++ components/tt/dropdown/UserProfile.vue
@@ -6,7 +6,7 @@
     :on-select="onSelect"
   >
     <NSkeleton v-if="!auth" class="h-14 mr-6" />
-    <NButton v-else :theme-overrides="buttonThemeOverrides" class="h-14 rounded px-4 py-2 text-sm font-semibold leading-6 text-white border border-red-300 cursor-default">
+    <NButton v-else :theme-overrides="buttonThemeOverrides" style="height: 3.5rem;" class="rounded px-4 py-2 text-sm font-semibold leading-6 text-white border border-red-300 cursor-default">
       <div class="flex items-center text-left gap-x-4">
         <NAvatar class="h-8 w-8 bg-slate-800" fallback-src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg">
           {{ auth?.name.substring(0, 2).toUpperCase() }}
